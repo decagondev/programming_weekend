7191e35d53450fa76a2192386cf67fb16a759f54
Tom <tomtarpeydev@gmail.com>
2020-02-23 14:47:20 +0000

    making change with memoization

--- python/scratchpad/making_change.py
+++ python/scratchpad/making_change.py
@@ -7,7 +7,20 @@ import sys
 #   making_change(11, [2, 3, 4, 5]) + making_change(20, [2, 3])
 
 
-def making_change(amount, denominations):
+# def making_change(amount, denominations):
+#     # base cases
+#     if amount == 0:
+#         return 1
+#     if amount < 0:
+#         return 0
+#     if len(denominations) <= 0 and amount > 0:
+#         return 0
+#     # recursive case
+#     else:
+#         return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[:-1])
+
+# with memoization
+def making_change(amount, denominations, cache = dict()):
     # base cases
     if amount == 0:
         return 1
@@ -15,9 +28,24 @@ def making_change(amount, denominations):
         return 0
     if len(denominations) <= 0 and amount > 0:
         return 0
+    
+    # memoization cache case
+    if (amount, len(denominations)) in cache.keys():
+        return cache[(amount, len(denominations))]
     # recursive case
     else:
-        return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[:-1])
+        waysToMakeChange = 0
+        for i in range(len(denominations)):
+            coin = denominations[i]
+            if coin > amount:
+                continue
+            else:
+                result = making_change(amount - coin, denominations[i:], cache)
+                if result > 0:
+                    waysToMakeChange += result
+        
+        cache[(amount, len(denominations))] = waysToMakeChange
+        return waysToMakeChange
 
 
 
